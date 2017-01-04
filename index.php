2e097333d3d42377412d91ff3ac7ebeb8d8dd8b7
y0sh <mail@Yoshi210.com>
2017-01-04 15:15:26 +0100

    Create index.php
    html zu php gewandelt für die funktionalität des kontaktvormulars

---
+++ index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Cryptoparty | Maschinendeck e.V.</title>
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css'>
+	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.8/typicons.min.css'>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <!-- Header Section ––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<header id="header">
+  <div class="main_nav">
+    <div class="container">
+      <div class="mobile-toggle"> <span></span> <span></span> <span></span> </div>
+      <nav>
+        <ul>
+			<li><a class="smoothscroll" href="#header">Home</a></li>
+			<li><a class="smoothscroll" href="#about">Über Cryptopartys</a></li>
+			<li><a class="smoothscroll" href="#portfolio">Verwendete Software</a></li>
+			<li><a class="smoothscroll" href="#termin">Termine</a></li>
+			<li><a class="smoothscroll" href="#team">Die Veranstallter</a></li>
+			<li><a class="smoothscroll" href="#testimonial">Kontakt</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div class="title">
+    <div><img src="images/cryptoparty.png"></div>
+    <div class="smallsep heading"></div>
+      <article>
+    	 <h1 id="headline">Maschinendeck e.V.</h1>
+    	  <p id="text">Wir lernen wie Verschlüsselungen funktionieren, E-Mails wie Edward Snowden zu verschlüsseln, beleuchten, was „Metadaten“ beim Surfen im Internet über uns preisgeben, lernen Datenträger (Festplatten, USB-Sticks) sowie Chats & Messenger zu verschlüsseln.</p>
+      </article>
+      <a class="smoothscroll" href="#about">
+    <div class="mouse">
+      <div class="wheel"></div>
+    </div>
+    </a> </div>
+  <a class="smoothscroll" href="#about">
+  <div class="scroll-down"></div>
+  </a> </header>
+
+<!-- About Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<section id="about">
+  <div class="container">
+    <div class="row">
+      <h1>Über Cryptopartys</h1>
+      <div class="block"></div>
+      <p>Cryptoparty? Was ist das überhaupt?</p>
+      <p>Bei einer Cryptoparty wird versucht, den Teilnehmern die Grundkenntnisse und Prinzipien der Verschlüsselung näher zu bringen, um somit immer mehr Menschen mit der sicheren Kommunikation im Netz vertraut zu machen. Dabei wird auf den Einsatz von freier und kostenloser Open-Source-Software sehr großen Wert gelegt.
+        <br>Dank den Enthüllungen von Edward Snowden ist nun klar, dass Geheimdienste wie NSA und BND die Bürger im Internet ausspionieren. Diese Spionage können wir zwar nicht verhindern, jedoch können wir Techniken einsetzen, die es den Geheimdiensten wesentlich schwieriger macht unsere Daten auszuwerten.
+        Wir stellen in Cryptopartys Programme, Tools und Hinweise vor, welche ohne großen Aufwand das Surfen im Internet sowie das versenden von E-Mails verschlüsselt.
+
+      <!--  Diesen Spionagen kann man jedoch gegensteuern, indem man einfache Programme einsetzt, dieDem kann man ohne großen aufwand entgegenwirken.
+-->
+      </p>
+<!--
+      <p>Das Ziel einer Cryptoparty ist es, Menschen die Grundlegenden Techniken der Verschlüsselung näher zu bringen. Zudem wird noch versucht, freie und kostenlose Software einzusetzen.</p>
+-->
+    </div>
+    <div class="row">
+      <div class="six columns">
+        <h3><span class="typcn typcn-key-outline icon"></span>Verschlüsselung von E-Mails</h3>
+        <p>Lernen, wie einfach die Verschlüsselung von E-Mails ist. Ohne großen Aufwand lassen sich E-Mails sicher Verschlüsseln, sodass keine Dritten den Inhalt dieser Mails auslesen können.</p>
+      </div>
+      <div class="six columns">
+        <h3><span class="typcn typcn-lock-closed-outline icon"></span>Anonymes surfen im Internet</h3>
+        <p>Mit Hilfe eines speziellen Browsers, dem TOR-Browser, anonym und sicher im Internet surfen. Im Laufe der Veranstaltung wird die besondere Funktionsweise von TOR und die Unterschiede zum "normalen" Internet vorgestellt.</p>
+      </div>
+      <div class="row">
+        <div class="six columns">
+          <h3><span class="typcn typcn-messages icon"></span>Sicheres Instant-Messaging</h3>
+          <p>Der weltweit am meisten genutzte Messager ist WhatsApp. Seit dem Kauf 2014 von Facebook und dem Datenaustausch zwischen Facebook und WhatsApp ist die Suche nach einem sicheren Messager immer größer geworden. In der Cryptoparty versuchen wir, sichere und freie Messanger vorzustellen sowie Argumente zum Überreden von Freunden und Familie bereit zu stellen.</p>
+        </div>
+        <div class="six columns">
+          <h3><span class="typcn typcn-image-outline icon"></span>Sicheres Aufbewahren der Urlaubsfotos</h3>
+          <p>Nichts ist peinlicher als den USB-Stick mit den Urlaubsfotos zu verlieren. Der Finder des USB-Sticks kann ohne Schwierigkeiten den kompletten Inhalt des Sticks auslesen und alle private Urlaubsfotos ansehen. Mit Hilfe von Festplattenverschlüsselung währe das nicht passiert. Darum wird in der Cryptoparty vorgestellt, USB-Sticks oder auch ganze Computerfestplatten sicher zu verschlüsseln, sodass Ein Zugriff ohne Passwort nicht möglich ist.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Skills Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<!--
+<section id="party">
+  <div class="container">
+    <h1>Die Party</h1>
+    <div class="block"></div>
+    <div class="row">
+      <div class="one-third column">
+        <h3>wie funmktioneirt das </h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus.</p>
+      </div>
+      <div class="one-third column">
+        <h3>was amchen wir hier</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus.</p>
+      </div>
+      <div class="one-third column">
+        <h3>damit ihr was lernt xD</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="eight columns">
+        <div class="progressBar">
+          <h4>Verschlüsselung</h4>
+          <div class="progressBarContainer">
+            <div class="progressBarValue value-90"></div>
+          </div>
+        </div>
+        <div class="progressBar">
+          <h4>Meta-Daten</h4>
+          <div class="progressBarContainer">
+            <div class="progressBarValue value-80"></div>
+          </div>
+        </div>
+        <div class="progressBar">
+          <h4>Das Interent</h4>
+          <div class="progressBarContainer">
+            <div class="progressBarValue value-30"></div>
+          </div>
+        </div>
+        <div class="progressBar">
+          <h4>Das Level 8 Problem</h4>
+          <div class="progressBarContainer">
+            <div class="progressBarValue value-70"></div>
+          </div>
+        </div>
+      </div>
+      <div class="four columns">
+        <p>Lorem ipsum dolor sit amet, enim soluta consectetur adipisicing elit. Sit, eius, itaque, porro, beatae impedit officia tenetur reiciendis in quia eum autem. Enim soluta consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  </div>
+</section>
+-->
+<!-- Portfolio Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<section id="portfolio">
+  <div class="container">
+    <h1>Verwendete Software</h1>
+    <div class="block"></div>
+    <p>Während dem Verlauf der Cryptoparty werden viele, verschiedene Programme und Software vorgestellt. Generell muss für die Teilnahme an einer Cryptoparty noch keine Software vorinstalliert werden, das wird während der Party mit allen Teilnehmern gemeinsam Schritt für Schritt installiert, konfiguriert und von den Mentoren erklärt.</p>
+    <div class="row">
+      <div class="three columns">
+        <h3>TOR-Browser</h3>
+        <img src="images/logo_Tor.png" class="image" alt="logo_Tor.png"/>
+        Mit dem TOR-Browser sicher und anonym im Internet surfen.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://www.torproject.org">https://www.torproject.org</a><br>
+      </div>
+      <div class="three columns">
+        <h3>GnuPG</h3>
+        <img src="images/logo_GnuPG.png" class="image" alt="logo_GnuPG.png"/>
+        Mit Hilfe von GnuPG, einer Open-Source-Version von PGP, können Dateien verschlüsselt werden.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://www.gnupg.org">https://www.gnupg.org</a><br>
+      </div>
+      <div class="three columns">
+        <h3>Enigmail</h3>
+        <img src="images/logo_Enigmail.png" class="image" alt="logo_Enigmail.png"/>
+        Enigmail, ein Add-On für Mozilla Thunderbird, verschlüsselt mit Hilfe von GnuPG E-Mails.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://www.enigmail.net">https://www.enigmail.net</a><br>
+      </div>
+      <div class="three columns">
+        <h3>VeraCrypt</h3>
+        <img src="images/logo_VeraCrypt.png" class="image" alt="logo_VerCrypt.png"/>
+        VeraCrypt wird eingesetzt, um USB-Sticks sowie Festplatten zu verschlüsseln.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://veracrypt.codeplex.com/">https://veracrypt.codeplex.com/</a><br>
+      </div>
+    </div>
+    <div class="row" style="margin-top: 30px;">
+      <div class="three columns">
+        <h3>Telegram</h3>
+        <img src="images/logo_Telegram.png" class="image" alt="logo_Telegram.png"/>
+        Telegram ist ein sicherer und quelloffener Instant-Messanger, verfügbar für Smartphones sowie Computer.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://telegram.org/">https://telegram.org/</a><br>
+      </div>
+      <div class="three columns">
+        <h3>Signal</h3>
+        <img src="images/logo_Signal.png" class="image" alt="logo_Signal.png"/>
+        Signal ist ein weiterer alternativer und sicherer Messanger, auch verfügbar für Smartphones sowie Computer.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://whispersystems.org/">https://whispersystems.org/</a><br>
+      </div>
+	  <div class="three columns">
+        <h3>Threema</h3>
+        <img src="images/logo_Threema.png" class="image" alt="logo_Signal.png"/>
+        Threema ist so konzipiert, dass möglichst keine Datenspur entsteht: Gruppen und Kontaktlisten werden auf Ihrem Gerät und nicht auf den Servern verwaltet. Nachrichten werden sofort nach Zustellung gelöscht.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://threema.ch/de">https://threema.ch/de/</a><br>
+      </div>
+	  <div class="three columns">
+        <h3>qTox</h3>
+        <br><img src="images/logo_qtox.png" class="image" alt="logo_Signal.png"/><br><br><br>
+        Tox ist eine Peer-to-Peer-Messaging-Software. Die Arbeit an diesem Projekt begann im Jahr 2013 - als Reaktion auf die globale Massenüberwachung durch Geheimdienste - mit dem Ziel, eine freie und quelloffene Skype-Alternative zu schaffen.<br>
+        <span class="bold">Weiterführende Links</span><br>
+        Projektwebseite: <a href="https://tox.chat/">https://tox.chat/</a><br>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Termine Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<section id="termin">
+  <div class="container">
+    <div class="row">
+      <h1>Termine</h1>
+      <div class="block"></div>
+<p>Das Maschinendeck aus Trier veranstaltet in Kooperation mit dem Bistum Trier am 25. November 2017, 14 bis 17 Uhr. im Haus der Jugend in Bitburg eine Cryptoparty.
+  Um besser Planen zu können bitten wir um eine Anmeldung bis zum 01.11.2017 via E-Mail an cryptoparty@pgp.space.<br>
+  Grundlegende Kenntnisse mit dem Umgang eines Computers sollten vorhanden sein. Eigene Notebooks bzw. Laptops sollten auf jeden Fall mitgebracht werden, um Praxisübungen während der Cryptoparty durchzuführen.
+  Teilnehmer ohne eigenen Laptop können trotzdem sehr gerne mitmachen, einige Geräte können wir auch zur Verfügung stellen.
+  Komplette Desktop-Systeme sollten aus Platzgründen jedoch nicht mitgebracht werden! Die oben aufgelisteten Programme müssen noch nicht vor der Cryptoparty installiert sein. Während der Party werden wir diese zusammen Installieren und Konfigurieren.
+</p>
+    </div>
+  </div>
+</section>
+
+<!-- Team Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<section id="team">
+  <div class="container">
+    <div class="row">
+      <h1>Die Veranstallter</h1>
+      <div class="block"></div>
+      <p>Vom Maschinendeck e.V. aus Trier sind zu gast, Julian "y0sh" und Niklas, auch "Energy" genannt, in Kooperation mit Rafael von der Fachstelle für Kinder und Jugendpastoral des Bistum Triers die Cryptoparty am 25. November 2017, 14 bis 17 Uhr. im Haus der Jugend in Bitburg.</p>
+    </div>
+    <div class="row">
+      <div class="three columns">
+        <h4>Julian | y0sh</h4>
+        <p>Mitglied im Maschinendeck e.V.</p>
+        <p>
+          <span class="bold">Kontakt</span><br>
+          <a href="http://y0sh.net" target="_blank"><span class="typcn typcn-home-outline icon"></span></a>
+          <a href="mailto:mail@y0sh.net" target="_blank"><span class="typcn typcn-mail icon"></span></a>
+          <a href="https://twitter.com/y_0_s_h" target="_blank"><span class="typcn typcn-social-twitter icon"></span></a>
+          <br> <a href="http://pgp.mit.edu/pks/lookup?op=get&search=0x0679CB2A1DF4D852">PGP-Key</a>
+        </p>
+      </div>
+      <div class="three columns">
+        <h4>Niklas | Energy</h4>
+        <p>Mitglied im Maschinendeck e.V.</p>
+        <p>
+          <span class="bold">Kontakt</span><br>
+          <a href="http://www.niklas-schmitt.de" target="_blank"><span class="typcn typcn-home-outline icon"></span></a>
+          <a href="mailto:niklas@niklas-schmitt.de" target="_blank"><span class="typcn typcn-mail icon"></span></a>
+          <a href="https://twitter.com/NikPWR" target="_blank"><span class="typcn typcn-social-twitter icon"></span></a>
+          <br> <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x48B2693339DA4F66">PGP-Key</a>
+        </p>
+      </div>
+        <div class="three columns">
+        <h4>Rafael</h4>
+        <p>Mitarbeiter der Fachstelle für Kinder und Jugendpastoral des Bistum Triers</p>
+        <p>
+          <span class="bold">Kontakt</span><br>
+          <a href="http://www.bistum-trier.de/fachstellejugend-bitburg/" target="_blank"><span class="typcn typcn-home-outline icon"></span></a>
+          <a href="mailto:rafael.stoll@bgv-trier.de" target="_blank"><span class="typcn typcn-mail icon"></span></a>
+        </p>
+    </div>
+  </div>
+</section>
+
+<!-- Testimonials Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<section id="testimonial">
+  <div class="container">
+    <div class="quoteLoop">
+      <blockquote class="quote"> <img src="images/maschinendeck.png" width="100" height="100" alt=""/>
+        <h5>&nbsp;<br>
+          &rdquo;Maschinendeck e.V., Trier&rdquo;<br>
+          <small>Hacker, Maker & Nerd Space in Trier</small></h5>
+      </blockquote>
+      <blockquote class="quote"> <img src="images/jugend_Bistum_Trier.gif" width="100" height="100" alt=""/>
+        <h5>&nbsp;<br>
+          &ldquo;In Zusammenarbeit mit dem Bistum Trier&rdquo;<br>
+          <small>Fachstelle für Kinder und Jugendpastoral Bitburg</small></h5>
+      </blockquote>
+    </div>
+  </div>
+</section>
+
+<!-- Contact Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<?php
+ 
+ // An welche Adresse sollen die Mails gesendet werden?
+ $zieladresse = 'cryptoparty@pgp.space';  
+ 
+ // Welche Adresse soll als Absender angegeben werden?
+ // (Manche Hoster lassen diese Angabe vor dem Versenden der Mail ueberschreiben)
+ $absenderadresse = 'webformular-cryptoparty@pgp.space'; 
+ 
+ // Welcher Absendername soll verwendet werden?
+ $absendername = 'Formmailer'; 
+ 
+ // Welchen Betreff sollen die Mails erhalten?
+ $betreff = 'Kontaktformular';
+ 
+ // Zu welcher Seite soll als "Danke-Seite" weitergeleitet werden?
+ // Wichtig: Sie muessen hier eine gueltige HTTP-Adresse angeben!
+ $urlDankeSeite = 'danke.htm'; 
+ 
+ // Welche(s) Zeichen soll(en) zwischen dem Feldnamen und dem angegebenen Wert stehen?
+ $trenner = ":\t"; // Doppelpunkt + Tabulator
+ 
+ /** 
+  * Ende Konfiguration
+  */ 
+ 
+ require_once "swiftmail/lib/swift_required.php"; // Swift initialisieren
+ 
+ if ($_SERVER['REQUEST_METHOD'] === "POST") {
+ 
+     $message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht.
+ 
+     $message
+         ->setFrom(array($absenderadresse => $absendername))
+         ->setTo(array($zieladresse)) // alternativ existiert setCc() und setBcc()
+         ->setSubject($betreff);
+ 
+     $mailtext = "";
+ 
+     foreach ($_POST as $name => $wert) {
+         if (is_array($wert)) {
+                 foreach ($wert as $einzelwert) {
+                 $mailtext .= $name.$trenner.$einzelwert."\n";
+             }
+         } else {
+             $mailtext .= $name.$trenner.$wert."\n";
+         }
+     }
+ 
+     $message->setBody($mailtext, 'text/plain');
+ 
+     $mailer = Swift_Mailer::newInstance(Swift_MailTransport::newInstance());
+     $result = $mailer->send($message);
+ 
+     if ($result == 0) {
+         die("Mail konnte nicht versandt werden.");
+     }
+ 
+     header("Location: $urlDankeSeite");
+     exit;
+ }
+ 
+ header("Content-type: text/html; charset=utf-8");
+ 
+?>
+
+<div class="container">
+    <h1>Kontakt</h1>
+    <div class="block"></div>
+        <p>Du hast noch Fragen zu Cryptopartys oder zu den Veranstaltern?<br>Melde dich bei uns:</p>
+         <form action="" method="post">
+            <div class="row">
+        <div class="six columns">
+          <label for="exampleRecipientInput">Name</label>
+          <input class="u-full-width" type="text" name="Versender">
+        </div>
+        <div class="six columns">
+          <label for="exampleEmailInput">E-Mail</label>
+          <input class="u-full-width" type="email" name="E-Mail">
+        </div>
+      </div>
+      <div class="row">
+        <label for="exampleMessage">Nachricht</label>
+        <textarea class="u-full-width"></textarea>
+        <input name="Bemerkungen" class="button-primary" type="submit" value="Absenden">
+      </div>
+         </form>
+		</div>
+
+<!-- Footer Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<footer>
+  <div class="container">
+    <div class="nine columns">
+      <p>Created with ♥ by Energy & y0sh <br> Maschinendeck e.V. - Hacker, Maker & Nerd Space in Trier</p>
+    </div>
+    <div class="three columns">
+      <a href="http://www.maschinendeck.org/" target="_blank"><span class="typcn typcn-home-outline socialIcons"></span></a>
+      <a href="mailto:kontakt@maschinendeck.org" target="_blank"><span class="typcn typcn-mail socialIcons"></span></a>
+      <a href="https://twitter.com/Maschinendeck_" target="_blank"><span class="typcn typcn-social-twitter-circular socialIcons"></span></a>
+    </div>
+  </div>
+</footer>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js'></script>
+
+    <script src="js/index.js"></script>
+    <script src="js/crypto-texty.js"></script>
+
+</body>
+</html>
